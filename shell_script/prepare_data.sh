653c4329b8d09557c333dde097a506f25b36694e
zengls <3186428803@qq.com>
2026-01-22 21:36:36 +0800

    a6000-office debug

--- shell_script/prepare_data.sh
+++ shell_script/prepare_data.sh
@@ -1,3 +1,4 @@
 mkdir -p data
-atomworks setup metadata data/metadata
-atomworks pdb sync data/pdb_data --pdb-id 1A0I --pdb-id 7XYZ
+uv run atomworks setup metadata data/metadata
+uv run atomworks pdb sync data/pdb_data --pdb-id 1A0I --pdb-id 7XYZ
+ln -s $(pwd)/data/metadata/pn_units_df.parquet data/metadata/pn_units_df_train.parquet
